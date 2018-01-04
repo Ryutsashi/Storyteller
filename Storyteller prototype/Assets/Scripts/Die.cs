2dbd634065e6f3c43835e60b9ec5be913da8c62c
Ryutsashi <ryutsashi@gmail.com>
2018-01-04 21:22:50 +0100

    Update v2018.1.4.0
    Situations file polished (almost done), die class written

---
+++ Storyteller prototype/Assets/Scripts/Die.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die {
+
+	public static int Roll ( int variance ) {
+		return Mathf.RoundToInt ( Random.Range ( 0f, 1f ) * variance );
+	}
+
+	public static int Roll ( int variance, Difficulty difficulty ) {
+		float result;
+		switch ( difficulty ) {
+			case Difficulty.hard:
+				result = ( Random.Range ( 0f, 1f ) + Random.Range ( 0f, 1f ) + Random.Range ( 0f, 1f ) ) / 3;
+				break;
+			case Difficulty.normal:
+				result = ( Random.Range ( 0f, 1f ) + Random.Range ( 0f, 1f ) ) * .5f;
+				break;
+			case Difficulty.easy:
+				result = Random.Range ( 0f, 1f );
+				break;
+			default:
+				result = Random.Range ( 0f, 1f );
+				break;
+		}
+		return Mathf.RoundToInt ( result * variance );
+	}
+}
+
+public enum Difficulty {
+	easy,
+	normal,
+	hard
+}
--- Storyteller prototype/Assets/Scripts/Situation.cs
+++ Storyteller prototype/Assets/Scripts/Situation.cs
@@ -5,28 +5,103 @@ using UnityEngine;
 [System.Serializable]
 public class Situation : ScriptableObject {
 
-	public SituationStep initialStep;
-	// requirements
-	// reward (modifier, equipment, quest)
+	// list of all situations
+	protected static List<Situation> allSituations;
+	public static List<Situation> AllSituations { get { return allSituations; } }
 
+	[SerializeField]
+	private SituationStep initialStep;
+	public SituationStep InitialStep { get { return initialStep; } }
+
+	// requirements
+
+	[SerializeField]
+	private List<Requirement> requirements;
+	public List<Requirement> Requirements { get { return requirements; } }
+
+	// TODO: reward (modifier, equipment, quest)
+
+	public Situation ( SituationStep initialStep, List<Requirement> requirements/*, List<RewardPackage> rewards*/ ) {
+		this.initialStep = initialStep;
+		this.requirements = requirements;
+	}
+
+	public bool CheckRequirements ( Character character ) {
+		foreach ( Requirement r in requirements ) {
+			return r.Test ( character );
+		}
+		// default to true if no requirements present
+		return true;
+	}
+
+	public void DeleteForever ( ) {
+		allSituations.Remove ( this );
+		// TODO: implement removing from json file
+	}
 }
 
 [System.Serializable]
 public class SituationStep {
 
-	// name of the option/situation
-	public string name;
+	// name of the situation
+	[SerializeField]
+	private string name;
+	public string Name { get { return name; } }
+
 	// description text
-	public string description;
+	[SerializeField]
+	private string description;
+	public string Description { get { return description; } }
+
 	// options
-	public List<SituationStep> options;
+	[SerializeField]
+	private List<SituationStepPackage> options;
+	public List<SituationStepPackage> Options { get { return options; } }
+
+	public SituationStep ( string name, string description, List<SituationStepPackage> options ) {
+		this.name = name;
+		this.description = description;
+		this.options = options;
+	}
+
+	public void DeleteForever ( ) {
+		// TODO: implement removing from json file
+	}
 }
 
 [System.Serializable]
 public class SituationStepPackage {
 
-	public SituationStep onSuccess;
-	public SituationStep onFailiure;
+	// name of the option package
+	[SerializeField]
+	private string name;
+	public string Name { get { return name; } }
+
+	// TODO: maybe add different levels of success/failiure eventually
+	[SerializeField]
+	private SituationStep successStep;
+	public SituationStep SuccessStep { get { return successStep; } }
+
+	[SerializeField]
+	private SituationStep failiureStep;
+	public SituationStep FailiureStep { get { return failiureStep; } }
+
 	// requirements
-	// reward (modifier, equipment)
+	[SerializeField]
+	private List<Requirement> requirements;
+	public List<Requirement> Requirements { get { return requirements; } }
+
+	// TODO: reward (modifier, equipment)
+	
+	public bool CheckRequirements ( Character character ) {
+		foreach ( Requirement r in requirements ) {
+			return r.Test ( character );
+		}
+		// default to true if no requirements present
+		return true;
+	}
+
+	public void DeleteForever ( ) {
+		// TODO: implement removing from json file
+	}
 }
